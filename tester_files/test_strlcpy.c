f5242421b836c4875e46e1aeaafa35acd1179a6f
Olena <lenanavorska@gmail.com>
2024-09-09 15:06:47 +0200

    fixinhg bsd

--- tester_files/test_strlcpy.c
+++ tester_files/test_strlcpy.c
@@ -6,6 +6,29 @@
 # include <stdio.h>
 # include <string.h>
 
+size_t  t_strlcpy(char *dst, const char *src, size_t dsize)
+{
+	const char *osrc = src;
+	size_t nleft = dsize;
+
+	/* Copy as many bytes as will fit. */
+	if (nleft != 0) {
+		while (--nleft != 0) {
+			if ((*dst++ = *src++) == '\0')
+				break;
+		}
+	}
+
+	/* Not enough room in dst, add NUL and traverse rest of src. */
+	if (nleft == 0) {
+		if (dsize != 0)
+			*dst = '\0';		/* NUL-terminate dst */
+		while (*src++)
+			;
+	}
+
+	return(src - osrc - 1);	/* count does not include NUL */
+}
 
 void test_strlcpy()
 {
@@ -14,7 +37,7 @@ void test_strlcpy()
     
     printf("\033[0;31mft_strlcpy:\033[0m");
     ft_strlcpy(dest, "Hello, World!", 13);
-    strlcpy(dest2, "Hello, World!", 13);
+    t_strlcpy(dest2, "Hello, World!", 13);
     if (strcmp(dest, dest2) != 0)
     {
         printf(" \033[0;31mKO \033[0m");
@@ -24,7 +47,7 @@ void test_strlcpy()
     bzero(dest, 100);
     bzero(dest2, 100);
     ft_strlcpy(dest, "Hello, World!", 0);
-    strlcpy(dest2, "Hello, World!", 0);
+    t_strlcpy(dest2, "Hello, World!", 0);
     if (strcmp(dest, dest2) != 0)
     {
         printf(" \033[0;31mKO \033[0m");
@@ -34,7 +57,7 @@ void test_strlcpy()
     bzero(dest, 100);
     bzero(dest2, 100);
     ft_strlcpy(dest, "Hello, World!", 5);
-    strlcpy(dest2, "Hello, World!", 5);
+    t_strlcpy(dest2, "Hello, World!", 5);
     if (strcmp(dest, dest2) != 0)
     {
         printf(" \033[0;31mKO\033[0m");
@@ -44,7 +67,7 @@ void test_strlcpy()
     bzero(dest, 100);
     bzero(dest2, 100);
     ft_strlcpy(dest, "", 15);
-    strlcpy(dest2, "", 15);
+    t_strlcpy(dest2, "", 15);
     if (strcmp(dest, dest2) != 0)
     {
         printf(" \033[0;31mKO\033[0m");
@@ -54,7 +77,7 @@ void test_strlcpy()
     bzero(dest, 100);
     bzero(dest2, 100);
     ft_strlcpy(dest, "Hello, World!", 15);
-    strlcpy(dest2, "Hello, World!", 15);
+    t_strlcpy(dest2, "Hello, World!", 15);
     if (strcmp(dest, dest2) != 0)
     {
         printf(" \033[0;31mKO\033[0m\n");
