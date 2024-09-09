b200526be2da279a856260c00ab32099cebe269d
Olena <lenanavorska@gmail.com>
2024-09-09 15:12:25 +0200

    fixinhg bsd

--- tester_files/test_strlcat.c
+++ tester_files/test_strlcat.c
@@ -6,6 +6,33 @@
 # include <stdio.h>
 # include <string.h>
 
+size_t  t_strlcat(char *dst, const char *src, size_t dsize)
+{
+	const char *odst = dst;
+	const char *osrc = src;
+	size_t n = dsize;
+	size_t dlen;
+
+	/* Find the end of dst and adjust bytes left but don't go past end. */
+	while (n-- != 0 && *dst != '\0')
+		dst++;
+	dlen = dst - odst;
+	n = dsize - dlen;
+
+	if (n-- == 0)
+		return(dlen + strlen(src));
+	while (*src != '\0') {
+		if (n != 0) {
+			*dst++ = *src;
+			n--;
+		}
+		src++;
+	}
+	*dst = '\0';
+
+	return(dlen + (src - osrc));	/* count does not include NUL */
+}
+
 void    test_strlcat()
 {   
     char    dest[100];
@@ -15,7 +42,7 @@ void    test_strlcat()
     bzero(dest, 100);
     bzero(dest2, 100);
     ft_strlcat(dest, "Hello, World!", 13);
-    strlcat(dest2, "Hello, World!", 13);
+    t_strlcat(dest2, "Hello, World!", 13);
     if (strcmp(dest, dest2) != 0)
     {
         printf(" \033[0;31mKO \033[0m");
@@ -24,7 +51,7 @@ void    test_strlcat()
     bzero(dest, 100);
     bzero(dest2, 100);
     ft_strlcat(dest, "Hello, World!", 0);
-    strlcat(dest2, "Hello, World!", 0);
+    t_strlcat(dest2, "Hello, World!", 0);
     if (strcmp(dest, dest2) != 0)
     {
         printf(" \033[0;31mKO \033[0m");
@@ -33,7 +60,7 @@ void    test_strlcat()
     bzero(dest, 100);
     bzero(dest2, 100);
     ft_strlcat(dest, "Hello, World!", 5);
-    strlcat(dest2, "Hello, World!", 5);
+    t_strlcat(dest2, "Hello, World!", 5);
     if (strcmp(dest, dest2) != 0)
     {
         printf(" \033[0;31mKO\033[0m");
@@ -42,7 +69,7 @@ void    test_strlcat()
     bzero(dest, 100);
     bzero(dest2, 100);
     ft_strlcat(dest, "", 15);
-    strlcat(dest2, "", 15);
+    t_strlcat(dest2, "", 15);
     if (strcmp(dest, dest2) != 0)
     {
         printf(" \033[0;31mKO\033[0m");
--- tester_files/test_strnstr.c
+++ tester_files/test_strnstr.c
@@ -4,6 +4,26 @@
 # include <stdio.h>
 # include <string.h>
 
+char *t_strnstr(const char *s, const char *find, size_t slen)
+{
+	char c, sc;
+	size_t len;
+
+	if ((c = *find++) != '\0') {
+		len = strlen(find);
+		do {
+			do {
+				if (slen-- < 1 || (sc = *s++) == '\0')
+					return (NULL);
+			} while (sc != c);
+			if (len > slen)
+				return (NULL);
+		} while (strncmp(s, find, len) != 0);
+		s--;
+	}
+	return ((char *)s);
+}
+
 void    test_strnstr()
 {
     char    *s1;
@@ -14,49 +34,49 @@ void    test_strnstr()
     s1 = "Hello World!";
     s2 = "World";
     n = 5;
-    if (ft_strnstr(s1, s2, n) == strnstr(s1, s2, n))
+    if (ft_strnstr(s1, s2, n) == t_strnstr(s1, s2, n))
         printf("\033[0;32mOK\033[0m");
     else
         printf("\033[0;31mKO\033[0m");
     s1 = "Hello World!";
     s2 = "World";
     n = 6;
-    if (ft_strnstr(s1, s2, n) == strnstr(s1, s2, n))
+    if (ft_strnstr(s1, s2, n) == t_strnstr(s1, s2, n))
         printf(" \033[0;32mOK\033[0m");
     else
         printf(" \033[0;31mKO\033[0m");
     s1 = "Hello World!";
     s2 = "World";
     n = 7;
-    if (ft_strnstr(s1, s2, n) == strnstr(s1, s2, n))
+    if (ft_strnstr(s1, s2, n) == t_strnstr(s1, s2, n))
         printf(" \033[0;32mOK\033[0m");
     else
         printf(" \033[0;31mKO\033[0m");
     s1 = "Hello World!";
     s2 = "World";
     n = 8;
-    if (ft_strnstr(s1, s2, n) == strnstr(s1, s2, n))
+    if (ft_strnstr(s1, s2, n) == t_strnstr(s1, s2, n))
         printf(" \033[0;32mOK\033[0m");
     else
         printf(" \033[0;31mKO\033[0m");
     s1 = "Hello World!";
     s2 = "World";
     n = 9;
-    if (ft_strnstr(s1, s2, n) == strnstr(s1, s2, n))
+    if (ft_strnstr(s1, s2, n) == t_strnstr(s1, s2, n))
         printf(" \033[0;32mOK\033[0m");
     else
         printf(" \033[0;31mKO\033[0m");
     s1 = "";
     s2 = "";
     n = 100;
-    if (ft_strnstr(s1, s2, n) == strnstr(s1, s2, n))
+    if (ft_strnstr(s1, s2, n) == t_strnstr(s1, s2, n))
         printf(" \033[0;32mOK\033[0m");
     else
         printf(" \033[0;31mKO\033[0m");
     s1 = "AAAAAA";
     s2 = "AAA";
     n = 6;
-    if (ft_strnstr(s1, s2, n) == strnstr(s1, s2, n))
+    if (ft_strnstr(s1, s2, n) == t_strnstr(s1, s2, n))
         printf(" \033[0;32mOK\033[0m");
     else
         printf(" \033[0;31mKO\033[0m");
